f57a09dc2bbe5c0f8300e1013d70f3b8f1fbdd7b
FORZpewpew <gachibassss@gmail.com>
2025-04-18 03:23:45 +0300

    report

--- app/prepare_index_data.py
+++ app/prepare_index_data.py
@@ -16,21 +16,20 @@ sc = spark.sparkContext
 data_rdd = sc.wholeTextFiles("hdfs:///data").cache()
 
 # Transform the RDD to extract id, title, and content
-# Assuming the file name format is id_title.txt
 transformed_rdd = data_rdd.map(lambda file: (
     os.path.basename(file[0]).split("_")[0],  # Extract id from the file name
     "_".join(os.path.basename(file[0]).split("_")[1:]).replace(".txt", ""),  # Extract title from the file name
     file[1]  # Content of the file
-)).map(lambda row: f"{row[0]}\t{row[1]}\t{row[2]}")  # Format as tab-separated string
+)).map(lambda row: f"{row[0]}\t{row[1]}\t{row[2]}")
 
 output_dir = "hdfs:///index/data"
 try:
-    subprocess.run(["hdfs", "dfs", "-rm", "-r", output_dir], check=True)
+    subprocess.run(["hdfs", "dfs", "-rm", "-r", output_dir], check=True)    # removing so that on consecutive builds it doesn't cause an error
     print(f"[LOGS] Removed existing output directory: {output_dir}")
 except subprocess.CalledProcessError:
     print(f"[LOGS] Output directory {output_dir} does not exist or could not be removed.")
 
-# Save the transformed RDD as a single partition to /index/data in HDFS
+# Save the transformed RDD as a single partition
 transformed_rdd = transformed_rdd.coalesce(1)
 
 transformed_rdd.saveAsTextFile("hdfs:///index/data/")
